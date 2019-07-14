1eaaab8af0d0bd6e9dbda02bbe4c380e965de6ca
Viraj Chavan <virajc@live.com>
2019-07-14 18:30:25 +0530

    show countDown in the popup as well

--- popup.js
+++ popup.js
@@ -1,22 +1,30 @@
+// saves the date
 document.getElementById('submitBtn').onclick = function() {
     let endDate = document.getElementById('endDate').value;
     chrome.storage.sync.set({endDate: endDate}, function() {
-      console.log("EndDate set to: " + endDate);
-      setDayCounter(new Date(endDate));
+      setBadgeValue(new Date(endDate));
+      showCounterOnPopup(new Date(endDate));
     });
 };
 
+// shows days left in the popup
 chrome.storage.sync.get('endDate', function(savedEndDate) {
   if(savedEndDate != undefined && savedEndDate != '') {
     document.getElementById('endDate').valueAsDate = new Date(savedEndDate.endDate);
+    showCounterOnPopup(savedEndDate.endDate);
   }
 });
 
-function setDayCounter(endDate) {
+function setBadgeValue(endDate) {
+  chrome.browserAction.setBadgeText({text: '' + getDaysLeft(endDate)});
+}
+
+function showCounterOnPopup(endDate) {
+  document.getElementById('days-left').innerHTML = getDaysLeft(new Date(endDate));
+}
+
+function getDaysLeft(endDate) {
   var today = new Date();
   var one_day = 1000*60*60*24;
-
-  var daysUntil = Math.ceil((endDate.getTime() - today.getTime())/one_day);
-
-  chrome.browserAction.setBadgeText({text: '' + daysUntil}); 
+  return Math.ceil((endDate.getTime() - today.getTime())/one_day);
 }

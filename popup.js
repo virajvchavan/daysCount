14015644f64b833f2cc76456449e5a97eeb3c836
Viraj Chavan <virajc@live.com>
2019-07-14 20:30:20 +0530

    input field validation

--- popup.js
+++ popup.js
@@ -1,14 +1,13 @@
 // saves the date
 document.getElementById('submitBtn').onclick = function() {
     let endDate = document.getElementById('endDate').value;
-    chrome.storage.sync.set({endDate: endDate}, function() {
-      setBadgeValue(new Date(endDate));
-      showCounterOnPopup(new Date(endDate));
-      document.getElementById('submitBtn').classList.add('disabled');
-      setTimeout(function() {
-        document.getElementById('submitBtn').classList.remove('disabled');
-      }, 400);
-    });
+
+    if (endDate != undefined && endDate != '') {
+      storeEndDate(endDate);
+      document.getElementById('endDate').classList.remove('error-input');
+    } else {
+      document.getElementById('endDate').classList.add('error-input');
+    }
 };
 
 window.addEventListener('keyup', function (e) {
@@ -19,11 +18,26 @@ window.addEventListener('keyup', function (e) {
 
 // get saved date from storage
 chrome.storage.sync.get('endDate', function(savedEndDate) {
-  if(savedEndDate != undefined && savedEndDate != '') {
+  if(savedEndDate != undefined && savedEndDate != '' && savedEndDate.endDate) {
     document.getElementById('endDate').valueAsDate = new Date(savedEndDate.endDate);
     showCounterOnPopup(savedEndDate.endDate);
+    showCountDown();
+  } else {
+    hideCountDown();
   }
 });
+
+function storeEndDate(endDate) {
+  chrome.storage.sync.set({endDate: endDate}, function() {
+    setBadgeValue(new Date(endDate));
+    showCounterOnPopup(new Date(endDate));
+
+    document.getElementById('submitBtn').classList.add('disabled');
+    setTimeout(function() {
+      document.getElementById('submitBtn').classList.remove('disabled');
+    }, 400);
+  });
+}
 
 function setBadgeValue(endDate) {
   var daysLeft = getDaysLeft(endDate);
@@ -35,14 +49,27 @@ function showCounterOnPopup(endDate) {
   var daysLeftElement = document.getElementById('days-left');
   daysLeftElement.innerHTML = getDaysLeft(new Date(endDate));
   daysLeftElement.classList.add('updated-date');
+  showCountDown();
+
   setTimeout(function() {
     daysLeftElement.classList.remove('updated-date');
     document.getElementById('submitBtn').classList.remove('disabled');
   }, 1000);
+
 }
 
 function getDaysLeft(endDate) {
   var today = new Date();
   var one_day = 1000*60*60*24;
   return Math.ceil((endDate.getTime() - today.getTime())/one_day);
+}
+
+function showCountDown() {
+  document.getElementsByClassName('countdown')[0].classList.remove('hidden');
+  document.getElementById('no-date-notice').classList.add('hidden');
+}
+
+function hideCountDown() {
+  document.getElementsByClassName('countdown')[0].classList.add('hidden');
+  document.getElementById('no-date-notice').classList.remove('hidden');
 }

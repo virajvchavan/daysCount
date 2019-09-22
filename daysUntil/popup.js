2f11a5330aece9a71bc1c5da4c879835d9ec23f6
Viraj Chavan <virajc@live.com>
2019-09-22 17:46:50 +0530

    refactored

--- daysUntil/popup.js
+++ daysUntil/popup.js
@@ -7,7 +7,7 @@ document.getElementById('submitBtn').onclick = function() {
 
 window.addEventListener('keyup', function (e) {
   if (e.keyCode === 13) {
-      document.getElementById('submitBtn').click();
+      saveTheInputs();
   }
 }, false);
 
